b479a2f915e4fc7ee6b1a755f4040e1f9acc8200
twiemold <twiemold@uvm.edu>
2022-04-07 11:00:14 -0400

    Defined class, added basic definitions.

--- src/Aligner.java
+++ src/Aligner.java
@@ -4,11 +4,24 @@ import java.util.function.BiFunction;
 public class Aligner {
 
     float delta;
-    BiFunction<Character, Character, Float> alpha;
+    BiFunction <Character, Character, Float> alpha;
+    int[][] alignmentArray;
 
     public Aligner(BiFunction<Character, Character, Float> comparator, float delta) {
-        alpha = comparator.apply(c1, c2);
-        delta = this.delta;
+        alpha = comparator;
+        this.delta = delta;
+    };
+
+    public float align(String s1, String s2) {
+        return 0;
+
+    };
+    private float opt(int i, int j) {
+        return 0;
+
+    };
+    private String traceback(int i, int j){
+        return "";
     };
 
 }
