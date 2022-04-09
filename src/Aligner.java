39ea4a8fc005926ca5c762bfbdac5d0eba1f2c9a
twiemold <twiemold@uvm.edu>
2022-04-09 14:37:23 -0400

    Project compiles and runs- results incorrect. Added strings to class def

--- src/Aligner.java
+++ src/Aligner.java
@@ -3,25 +3,54 @@ import java.util.function.BiFunction;
 
 public class Aligner {
 
+    final int arraySize = 50;
+
     float delta;
-    BiFunction <Character, Character, Float> alpha;
-    int[][] alignmentArray;
+    BiFunction <Character, Character, Float> comparator;
+    float[][] alignmentArray = new float[arraySize][arraySize];
+    String s1;
+    String s2;
 
     public Aligner(BiFunction<Character, Character, Float> comparator, float delta) {
-        alpha = comparator;
+        this.comparator = comparator;
         this.delta = delta;
     };
 
     public float align(String s1, String s2) {
-        return 0;
-
+        this.s1 = s1;
+        this.s2 = s2;
+        // m is length of s1
+        // n is length of s2
+        int i, j;
+        // Initialize A[i][0] = i*delta for each i
+        for (i = 0; i < s1.length(); ++i) {
+            alignmentArray[i][0] = (i * delta);
+        }
+        // Initialize A[0][j] = j*delta for each j
+        for (j = 0; j < s2.length(); ++j) {
+            alignmentArray[0][j] = (j * delta);
+        }
+        // for each j = 1 to n
+        for (j = 1; j < s2.length(); ++j) {
+            // for each i = 1 to m
+            for (i = 1; i < s1.length(); ++i) {
+                // opt(i, j);
+                opt(i, j);
+            }
+        }
+        System.out.println(alignmentArray[s1.length()-1][s2.length()-1]);
+        return alignmentArray[s1.length()-1][s1.length()-1];
     };
     private float opt(int i, int j) {
-        return 0;
-
+        if (alignmentArray[i][j] == 0 && i != 0 && j != 0) {
+            // apply lambda function
+            float alpha = comparator.apply(s1.charAt(i), s2.charAt(j));
+            alignmentArray[i][j] = Math.min(Math.min(alpha + opt(i - 1, j - 1), delta + opt(i - 1,j)), delta + opt(i, j - 1));
+        }
+        return alignmentArray[i][j];
     };
-    private String traceback(int i, int j){
-        return "";
+    private String traceback(int i, int j) {
+        return String.format("%s align withs %s", s1.charAt(i), s2.charAt(j));
     };
 
 }
