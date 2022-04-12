f87fdc0ebed2038fdadc28ceeec380d1f672fc78
twiemold <twiemold@uvm.edu>
2022-04-12 10:38:59 -0400

    Formatting, getting ready for submission

--- src/Aligner.java
+++ src/Aligner.java
@@ -1,15 +1,21 @@
+/**
+ * Thomas Wiemold
+ * 4/12/2022
+ * CS224
+ * Programming Assignment #5
+ * Sequence Alignment using Dynamic Programming
+ */
 
 import java.util.function.BiFunction;
 
 public class Aligner {
-
     final int arraySize = 50;
 
+    String s1;
+    String s2;
     float delta;
     BiFunction <Character, Character, Float> comparator;
     float[][] alignmentArray = new float[arraySize][arraySize];
-    String s1;
-    String s2;
 
     public Aligner(BiFunction<Character, Character, Float> comparator, float delta) {
         this.comparator = comparator;
@@ -19,32 +25,23 @@ public class Aligner {
     public float align(String s1, String s2) {
         this.s1 = s1;
         this.s2 = s2;
-        // m is length of s1
-        // n is length of s2
         int i, j;
-        // Initialize A[i][0] = i*delta for each i
         for (i = 0; i < s1.length()+1; ++i) {
             alignmentArray[i][0] = (i * delta);
         }
-        // Initialize A[0][j] = j*delta for each j
         for (j = 0; j < s2.length()+1; ++j) {
             alignmentArray[0][j] = (j * delta);
         }
-        // for each j = 1 to n
         for (j = 1; j < s2.length()+1; ++j) {
-            // for each i = 1 to m
             for (i = 1; i < s1.length()+1; ++i) {
-                // opt(i, j);
                 opt(i, j);
             }
         }
-        System.out.println(alignmentArray[s1.length()][s2.length()]);
         System.out.println(traceback(s1.length(), s2.length()));
         return alignmentArray[s1.length()][s1.length()];
     }
     private float opt(int i, int j) {
         if (alignmentArray[i][j] == 0 && i != 0 && j != 0) {
-            // apply lambda function
             float alpha = comparator.apply(s1.charAt(i-1), s2.charAt(j-1));
             alignmentArray[i][j] = Math.min(Math.min(alpha + opt(i - 1, j - 1), delta + opt(i - 1,j)), delta + opt(i, j - 1));
         }
@@ -52,9 +49,9 @@ public class Aligner {
     }
 
     private String traceback(int i, int j) {
-        String alignmentString = "";
+        StringBuilder alignmentString = new StringBuilder();
         while (i > 0 && j > 0) {
-            String lineString = "";
+            String lineString;
             float current = alignmentArray[i][j];
             float diagonal = alignmentArray[i-1][j-1];
             float vertical = alignmentArray[i][j-1];
@@ -72,9 +69,8 @@ public class Aligner {
                 i--;
                 lineString = String.format("%s aligns with -\n", s1.charAt(i));
             }
-            alignmentString += lineString;
+            alignmentString.append(lineString);
         }
-        return alignmentString;
+        return alignmentString.toString();
     }
-
 }
